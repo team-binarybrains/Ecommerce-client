c145d12b2002e712c1a672e9ad9f34948dfaa32f
Md.Mahabubul Kobir <96817149+MK-Sohan@users.noreply.github.com>
2022-09-13 21:26:18 +0600

    link

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-300 text-base-content">
+    <footer className="footer p-10 bg-base-300 text-base-content ">
       <div>
         <span className="footer-title">Services</span>
         <a className="link link-hover">Branding</a>
