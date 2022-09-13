a14e2fe2c4d6958f6dc7bf239df8b125248c96ab
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-09-13 19:51:41 +0600

    cart drawer designed

--- src/Component/Header/Cart/Cart.js
+++ src/Component/Header/Cart/Cart.js
@@ -7,7 +7,7 @@ const Cart = () => {
     [drawer,setDrawer] = useState(false);
 
     return (
-        <div className={`fixed w-screen backdrop-blur-sm top-[72px] lg:top-[117px] bottom-0 left-0 right-0 z-[99] ${drawer ? 'block': 'hidden'}`}>
+        <div className={`fixed w-screen backdrop-blur-sm top-[70px] lg:top-[117px] bottom-0 left-0 right-0 z-[99] ${drawer ? 'block': 'hidden'}`}>
             
             <section className={`relative w-full sm:w-[25rem] bg-white h-full ${drawer ? style.slideIn : style.slideOut}`}>
                 <button className='absolute top-5 right-5 btn' onClick={()=> {setDrawer(false)}}>X</button>
