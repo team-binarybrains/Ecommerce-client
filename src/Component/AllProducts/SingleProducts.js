e9f0a4e1b6cb6cb11a290a1528dc890e0c713fe0
Md.Mahabubul Kobir <96817149+MK-Sohan@users.noreply.github.com>
2022-09-13 23:03:43 +0600

    n

--- src/Component/AllProducts/SingleProducts.js
+++ src/Component/AllProducts/SingleProducts.js
@@ -7,15 +7,17 @@ const SingleProducts = ({ value }) => {
     navigate("/productDetail");
   };
   return (
-    <div className="bg-white border  shadow-lg p-5 rounded-lg rounded-tl-[90px] w-full max-w-[400px] mx-auto cursor-pointer hover:shadow-2xl transition">
+    <div className="bg-white border relative  shadow-lg p-5 rounded-lg rounded-tl-[90px] w-full max-w-[400px] mx-auto cursor-pointer hover:shadow-2xl transition">
       <img
         onClick={handleDetail}
-        className="mb-8 rounded-tl-[80px] "
+        className="mb-2 rounded-tl-[80px] "
         src={value.image}
         alt=""
       />
 
-      <div className="text-xl font-semibold max-w-[260px]">{value.name}</div>
+      <div className="text-xl font-semibold max-w-screen h-[50px]">
+        {value.name}
+      </div>
       <div className="text-lg font-semibold text-violet-600 mb-4 mt-2">
         <span className="text-3xl font-bold">৳</span> {value.price}
       </div>
@@ -25,8 +27,8 @@ const SingleProducts = ({ value }) => {
           {value.details.slice(0, 50)}...
         </h1>
       </div>
-      <div>
-        <button className="w-full tracking-wider p-5 rounded-xl rounded-t-none bg-clr font-bold   mt-5 text-white hover:text-dark transition duration-300">
+      <div className="mt-20 ">
+        <button className="w-full lg:w-[370px] mx-auto p-5 absolute bottom-0 lg:bottom-4 left-0 right-0 tracking-wider rounded-xl rounded-t-none bg-clr font-bold   mt-5 text-white hover:text-dark transition duration-300">
           অর্ডার করুন
         </button>
       </div>
