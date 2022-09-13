ef636d322ea8bfcf1ab9127499f8e7fdb954198d
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-09-14 00:50:58 +0600

    loading component added

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -8,6 +8,7 @@ import MailAndMssg from './MailAndMssg';
 import SocialAndLogin from './SocialAndLogin';
 import MobileMenu from './MobileMenu/MobileMenu';
 import Cart from './Cart/Cart';
+import Loading from '../Share/Loading';
 
 const Header = () => {
     const [menu,setMenu] = useState(false);
@@ -37,7 +38,7 @@ const Header = () => {
                 <MobileMenu menu={menu} setMenu={setMenu} className='block lg:hidden'/>
 
                 <Cart setMenu={setMenu}/>
-
+                
             </section>
 
         </div>
