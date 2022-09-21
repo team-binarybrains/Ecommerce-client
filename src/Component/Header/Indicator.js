66e6f2b71020959cdc7303709126afc39941970f
neyaznafiz <niiyazobug@gmail.com>
2022-09-21 18:18:05 +0600

    clear

--- src/Component/Header/Indicator.js
+++ src/Component/Header/Indicator.js
@@ -7,7 +7,7 @@ const Indicator = ({ className,setMenu,setDrawer }) => {
     return (
         <div className={className}>
 
-                <BsFillBookmarkHeartFill className='w-6 h-6 lg:text-gray-100 cursor-pointer' />
+                {/* <BsFillBookmarkHeartFill className='w-6 h-6 lg:text-gray-100 cursor-pointer' /> */}
 
                 <MdShoppingCart className='w-7 h-7 lg:text-gray-100 cursor-pointer' onClick={()=> {setDrawer(prev=>!prev);setMenu(false)}}/>
                 
