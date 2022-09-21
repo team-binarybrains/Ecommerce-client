038c9929e7bd66240d4da407025371a09a6b1266
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-09-21 16:01:33 +0600

    product details route button functionality added

--- src/Component/Home/HomepageProducts/HomePageProducts.js
+++ src/Component/Home/HomepageProducts/HomePageProducts.js
@@ -1,7 +1,7 @@
 import React from "react";
+import SingleProducts from "../../AllProducts/SingleProducts";
 import useAllProducts from "../../Hooks/useAllProducts";
 import Loading from "../../Share/Loading";
-import SingleHomepageProducts from "../SingleHomepageProducts/SingleHomepageProducts";
 
 const HomePageProducts = () => {
   const [products, loading] = useAllProducts();
@@ -10,7 +10,7 @@ const HomePageProducts = () => {
     <div className="grid grid-cols-1 p-4 lg:p-0 lg:grid-cols-3 gap-8 mt-10 mb-20">
       {loading && <Loading />}
       {products.map((value) => (
-        <SingleHomepageProducts key={value._id} value={value} />
+        <SingleProducts key={value.id} value={value} />
       ))}
     </div>
   );
