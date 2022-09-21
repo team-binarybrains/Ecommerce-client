e332e3bcaa4516b9f5842afa4f3b09d78dc79900
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-09-21 14:03:22 +0600

    cookie saved for 72hours

--- src/Component/Hooks/useCookie.js
+++ src/Component/Hooks/useCookie.js
@@ -5,8 +5,8 @@ const getCookie = (phone) => {
 
 const setCookie = (key)=> {
     const d = new Date();
-    d.setTime(d.getTime() + (45 * 1000));
-    // d.setTime(d.getTime() + (3 * 24 * 60 * 60 * 1000));
+    // d.setTime(d.getTime() + (45 * 1000));
+    d.setTime(d.getTime() + (3 * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
     document.cookie = key + "=" + true + ";" + expires + ";path=/";
 }
